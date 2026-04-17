dd3c3fb5ac9d32d588ff98bd5c34a39f5ed89871
architadoshi29 <architadoshi290@gmail.com>
2026-04-17 21:59:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PaginatedTable from './PaginatedTable';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-semibold text-center mt-8">Paginated User Data Table</h1>
+      <PaginatedTable />
+    </div>
+  );
+}
+
+export default App;
